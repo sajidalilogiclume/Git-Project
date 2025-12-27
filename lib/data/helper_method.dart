e2a32605f4136a3871ffe123669a45cf3227c431
sajidalilogiclume <sajidali@logiclume.com>
2025-12-27 09:25:45 +0500

    list

---
+++ lib/data/helper_method.dart
@@ -0,0 +1,12 @@
+import 'package:git_project/data/model/user_model.dart';
+
+class HelperMethod{
+  List<UserModel> getUserList(){
+    return [
+      UserModel(name: 'sajid', skill: 'android developer'),
+      UserModel(name: 'akber', skill: 'flutter developer'),
+      UserModel(name: 'asim', skill: 'react developer'),
+      UserModel(name: 'amir', skill: 'python developer')
+    ];
+  }
+}

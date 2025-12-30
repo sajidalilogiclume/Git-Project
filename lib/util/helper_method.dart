22f65fe094748162ad3bc1fd5eb167f7fc2d5e3e
sajidalilogiclume <sajidali@logiclume.com>
2025-12-30 12:47:17 +0500

    helper one

--- lib/util/helper_method.dart
+++ lib/util/helper_method.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:git_project/data/model/user_model.dart';
 class HelperMethod{
-  List<UserModel> getUserList(){
+ static List<UserModel> getUserList(){
     return [
       UserModel(name: 'sajid', skill: 'android developer'),
       UserModel(name: 'akber', skill: 'flutter developer'),
       UserModel(name: 'asim', skill: 'react developer'),
       UserModel(name: 'amir', skill: 'python developer')
     ];
+  }
+ static void appNavigator(BuildContext context,Widget screen){
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>screen));
   }
 }
